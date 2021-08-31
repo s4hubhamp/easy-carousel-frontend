11ed71f322b946fb4e1eaac935b77808bb36a719
Shubham <pwrs.0007@gmail.com>
2021-08-31 09:46:34 +0530

    added header and spinner components

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import Spinner from "../spinner/Spinner";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="carousel">
+      <h2>Easy Carousel</h2>
+      <Spinner />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,11 @@
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="lds-hourglass"></div>
+    </div>
+  );
+};
+
+export default Spinner;
