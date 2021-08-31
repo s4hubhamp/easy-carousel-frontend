35bb20bc04e7b9dc22c9c3731ba31d688cbaf77d
Shubham <pwrs.0007@gmail.com>
2021-08-31 10:02:54 +0530

    style improvement

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,10 +4,13 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="carousel">
-      <h2>Easy Carousel</h2>
-      <Spinner />
-    </div>
+    <>
+      <div className="carousel">
+        <h2>Easy Carousel</h2>
+        <Spinner />
+      </div>
+      <hr />
+    </>
   );
 };
 
